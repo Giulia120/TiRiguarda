995cb18556235d65f79a20a119e428f977e0ff42
Giulia120 <giulsuni@gmail.com>
2026-07-15 12:54:57 +0200

    Prova

---
+++ src/main/java/it/TiRiguarda/domain/Prova.java
@@ -0,0 +1,19 @@
+package it.TiRiguarda.domain;
+
+
+public class Prova {
+	private final int id;
+
+
+	public Prova(int id) {
+		this.id = id;
+		}
+
+
+	public int getId() {
+		return id;
+	}
+	
+	
+}
+
