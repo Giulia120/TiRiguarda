6e9522f4e63c6c252c4c6f69ac8dd530bee743c4
Caterina7 <spinellicaterina11@gmail.com>
2026-07-16 11:48:00 +0200

    classe protocolloPrEP

---
+++ src/main/java/it/TiRiguarda/domain/ProtocolloPrEP.java
@@ -0,0 +1,26 @@
+package it.TiRiguarda.domain;
+
+public class ProtocolloPrEP {
+	private Utente utente;
+	private final TipologiaPrEP tipoPrEP;
+	private final boolean statoPrEP;
+	
+	public ProtocolloPrEP(Utente utente, TipologiaPrEP tipoPrEP, boolean statoPrEP) {
+		this.utente = utente;
+		this.tipoPrEP = tipoPrEP;
+		this.statoPrEP = statoPrEP;
+	}
+	
+	public Utente getUtente() {
+		return utente;
+	}
+	
+	public TipologiaPrEP getTipoPrEP() {
+		return tipoPrEP;
+	}
+	
+	public boolean getStatoPrEP() {
+		return statoPrEP;
+	}
+	
+}
---
+++ src/main/java/it/TiRiguarda/domain/TipologiaPrEP.java
@@ -0,0 +1,6 @@
+package it.TiRiguarda.domain;
+
+public enum TipologiaPrEP {
+	Daily,
+	On_Demand;
+}
