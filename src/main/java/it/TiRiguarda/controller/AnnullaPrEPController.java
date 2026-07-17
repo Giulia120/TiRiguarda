0533806e26c02e5ae059ddab7faf965879fc11b5
Caterina7 <spinellicaterina11@gmail.com>
2026-07-17 10:16:36 +0200

    Annulla prep controller

--- src/main/java/it/TiRiguarda/controller/AnnullaPrEPController.java
+++ src/main/java/it/TiRiguarda/controller/AnnullaPrEPController.java
@@ -9,7 +9,7 @@ public class AnnullaPrEPController {
 	public boolean verificaStatoPrEP(ProtocolloPrEPBean bean) throws Exception{
 		boolean stato = bean.getStatoPrEP();
 		
-		if (stato = false ) {
+		if (stato == false ) {
 			
 		}
 	}
