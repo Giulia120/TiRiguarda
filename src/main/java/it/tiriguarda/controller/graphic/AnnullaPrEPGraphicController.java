b5fb84cca66556fea944c06a7e2d0dc57eb470be
Caterina7 <spinellicaterina11@gmail.com>
2026-07-19 17:26:52 +0200

    Grafic Controller

---
+++ src/main/java/it/tiriguarda/controller/graphic/AnnullaPrEPGraphicController.java
@@ -0,0 +1,33 @@
+package it.tiriguarda.controller.graphic;
+
+import it.tiriguarda.controller.app.AnnullaPrEPController;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class AnnullaPrEPGraphicController {
+	
+	@FXML
+	private Button siConfermaAnnullamento;
+	
+	@FXML
+	private Button noConfermaAnnullamento;
+	
+	@FXML
+	private Button menuPrincipale;
+	
+	@FXML
+	public void onConfermaAnnullamento(ActionEvent event) {
+		
+		AnnullaPrEPController controller = new AnnullaPrEPController();
+		
+		try {
+			controller.annullaPrEP();
+			//schermata di successo
+			
+		}catch(Exception e) {
+			//errore
+		}
+		
+	}
+}
