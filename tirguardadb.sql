862ef59d18b3c547c8476f982eb9244a26f81703
Caterina7 <spinellicaterina11@gmail.com>
2026-07-27 11:38:06 +0200

    db

--- tirguardadb.sql
+++ tirguardadb.sql
@@ -5,12 +5,43 @@ USE `tiriguardadatabase`;
 DROP TABLE IF EXISTS `tiriguardadatabase`.`Utente`;
 CREATE TABLE `tiriguardadatabase`.`Utente`(
 	`username` VARCHAR(45) NOT NULL,
-    `password` CHAR(32) NOT NULL,
+    `password` CHAR(64) NOT NULL,
     `sessoBiologico` ENUM('Femminile', 'Maschile') NOT NULL,
     `numeroTelefono` VARCHAR(50) NOT NULL,
     `protocolloAttivo` VARCHAR(45) DEFAULT NULL,
     PRIMARY KEY (`username`))
 ENGINE = InnoDB;
+
+DROP TABLE IF EXISTS `tiriguardadatabase`.`ProtocolloPrEP`;
+CREATE TABLE `tiriguardadatabase`.`ProtocolloPrEP`(
+	`idProtocollo` VARCHAR(45) NOT NULL,
+    `utente` VARCHAR(45) NOT NULL,
+    `tipoPrEP` ENUM('Daily', 'On_Demand') NOT NULL,
+    `dataInizio` DATE NOT NULL,
+    `statoPrEP` BOOLEAN NOT NULL DEFAULT 1,
+    `dataFine` DATE,
+    PRIMARY KEY (`idProtocollo`))
+ENGINE = InnoDB;
+
+DROP TABLE IF EXISTS `tiriguardadatabase`.`Rapporto`;
+CREATE TABLE `tiriguardadatabase`.`Rapporto`(
+    `utente` VARCHAR(45) NOT NULL,
+    `idRapporto` VARCHAR(45) NOT NULL,
+    `data` DATE NOT NULL,
+    `rischio` ENUM('Nullo', 'Basso', 'Alto') NOT NULL,
+    `dataFinePeriodoFinestra` DATE,
+    PRIMARY KEY (`idRapporto`))
+ENGINE = InnoDB;
+
+DROP TABLE IF EXISTS `tiriguardadatabase`.`Test`;
+CREATE TABLE `tiriguardadatabase`.`Test`(
+    `utente` VARCHAR(45) NOT NULL,
+    `idTest` VARCHAR(45) NOT NULL,
+    `tipoTest` ENUM('Prelievo', 'Rapido') NOT NULL,
+    `data` DATE NOT NULL,
+    PRIMARY KEY (`idTest`))
+ENGINE = InnoDB;
+
 
 
 
