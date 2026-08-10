7ee16435d7831508c434155e8121bb66cdecde17
Caterina7 <spinellicaterina11@gmail.com>
2026-08-10 18:44:02 +0200

    test protocollo prep

--- src/test/java/it/tiriguarda/controller/app/TestConfiguraPrEPAppController.java
+++ src/test/java/it/tiriguarda/controller/app/TestConfiguraPrEPAppController.java
@@ -1,11 +1,11 @@
 package it.tiriguarda.controller.app;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,54 +16,103 @@ import it.tiriguarda.dao.DAOFactory;
 import it.tiriguarda.dao.DAOFactoryProvider;
 import it.tiriguarda.dao.ProtocolloPrEPDAO;
 import it.tiriguarda.dao.UtenteDAO;
+import it.tiriguarda.domain.Precauzioni;
 import it.tiriguarda.domain.ProtocolloPrEP;
 import it.tiriguarda.domain.SessoBiologico;
+import it.tiriguarda.domain.TipoRapporto;
 import it.tiriguarda.domain.TipologiaPrEP;
 import it.tiriguarda.domain.Utente;
 import it.tiriguarda.dto.ProtocolloPrEPBean;
+import it.tiriguarda.dto.RapportoBean;
 import it.tiriguarda.exception.ProtocolloAttivoException;
 import it.tiriguarda.service.SessionManager;
 
-public class TestConfiguraPrEPAppController {
-	private ConfiguraPrEPAppController controller;
-	private Utente utenteTest;
-	
-	@BeforeEach
-	public void setUp() {
-		controller = new ConfiguraPrEPAppController();
-		utenteTest = new Utente("Anna", "password", SessoBiologico.FEMMINILE, "342776990");
-		SessionManager.getInstance().setUtenteLoggato(utenteTest);
-		UtenteDAO utenteDAO = DAOFactoryProvider.getDAOFactory().createUtenteDAO();
-		utenteDAO.registraUtente(utenteTest);
-	}
-	
-	@AfterEach
-	public void tearDown() {
-		SessionManager.getInstance().clearSessione();
-		ProtocolloPrEPDAO dao = DAOFactoryProvider.getDAOFactory().createProtocolloPrEPDAO();
-		ProtocolloPrEP protocollo = dao.trovaProtocolloAttivo(utenteTest.getUsername());
-		if (protocollo != null) {
-			dao.annullaStatoProtocollo(protocollo);
+	public class TestConfiguraPrEPAppController {
+		private ConfiguraPrEPAppController controller;
+		private Utente utenteTest;
+		
+		@BeforeEach
+		public void setUp() {
+			controller = new ConfiguraPrEPAppController();
 		}
-	}
-	
-	@Test
-	@DisplayName("Configura una PrEP On Demand con data di fine corretta")
-	public void testConfiguraPrEPOndemand() throws Exception{
-		SessionManager.getInstance();
-		ProtocolloPrEPBean beanTest = new ProtocolloPrEPBean();
 		
-		beanTest.setTipoPrEP(TipologiaPrEP.ON_DEMAND);
-		LocalDate dataInizio = LocalDate.now().plusDays(1);
-		beanTest.setDataInizio(dataInizio);
-		beanTest.setOrario(LocalTime.of(10, 0));
+		@AfterEach
+		public void tearDown() {
+			SessionManager.getInstance().clearSessione();
+			ProtocolloPrEPDAO dao = DAOFactoryProvider.getDAOFactory().createProtocolloPrEPDAO();
+			ProtocolloPrEP protocollo = dao.trovaProtocolloAttivo(utenteTest.getUsername());
+			if (protocollo != null) {
+				dao.annullaStatoProtocollo(protocollo);
+			}
+		}
+		private void fakeUtente(String username) {
+			utenteTest = new Utente(username , "password", SessoBiologico.FEMMINILE, "342776990");
+			SessionManager.getInstance().setUtenteLoggato(utenteTest);
+		}
 		
-		controller.configuraPrEP(beanTest);
-		
-		DAOFactory factory = DAOFactoryProvider.getDAOFactory();
-		ProtocolloPrEPDAO dao = factory.createProtocolloPrEPDAO();
-		ProtocolloPrEP protocollo = dao.trovaProtocolloAttivo(utenteTest.getUsername());
+		@Test
+		@DisplayName("Configura una PrEP On Demand con data di fine corretta")
+		public void testConfiguraPrEPOndemand() throws Exception{
+			fakeUtente("Utente1");
+			ProtocolloPrEPBean beanTest = new ProtocolloPrEPBean();
+			
+			beanTest.setTipoPrEP(TipologiaPrEP.ON_DEMAND);
+			LocalDate dataInizio = LocalDate.now().plusDays(1);
+			beanTest.setDataInizio(dataInizio);
+			beanTest.setOrario(LocalTime.of(10, 0));
+			
+			controller.configuraPrEP(beanTest);
+			
+			DAOFactory factory = DAOFactoryProvider.getDAOFactory();
+			ProtocolloPrEPDAO dao = factory.createProtocolloPrEPDAO();
+			ProtocolloPrEP protocollo = dao.trovaProtocolloAttivo(utenteTest.getUsername());
 
-		assertEquals(dataInizio.plusDays(7), protocollo.getDataFine());
-	}
+			assertEquals(dataInizio.plusDays(7), protocollo.getDataFine());
+		}
+		
+		@Test
+		@DisplayName("Lancia eccezione se si tenta di configurare una PrEP con un protocollo già attivo")
+		public void testConfiguraPrEPConProtocolloAttivoLanciaEccezione() throws Exception {
+			fakeUtente("Utente2");
+			ProtocolloPrEPBean beanPrimo = new ProtocolloPrEPBean();
+			beanPrimo.setTipoPrEP(TipologiaPrEP.ON_DEMAND);
+			beanPrimo.setDataInizio(LocalDate.now().plusDays(1));
+			beanPrimo.setOrario(LocalTime.of(10, 0));
+			
+			controller.configuraPrEP(beanPrimo);
+			
+			ProtocolloPrEPBean beanSecondo = new ProtocolloPrEPBean();
+			beanSecondo.setTipoPrEP(TipologiaPrEP.ON_DEMAND);
+			beanSecondo.setDataInizio(LocalDate.now().plusDays(2));
+			beanSecondo.setOrario(LocalTime.of(14, 0));
+			
+			assertThrows(ProtocolloAttivoException.class, () -> controller.configuraPrEP(beanSecondo));
+		}
+		
+		@Test
+		@DisplayName("Lancia eccezione se si tenta di configurare una PrEP senza specificare l'orario")
+		public void testConfiguraPrEPSenzaOrarioLanciaEccezione() throws Exception{
+			fakeUtente("Utente3");
+			ProtocolloPrEPBean beanTest = new ProtocolloPrEPBean();
+			beanTest.setTipoPrEP(TipologiaPrEP.ON_DEMAND);
+			beanTest.setDataInizio(LocalDate.now().plusDays(1));
+			
+			assertThrows(Exception.class, () -> controller.configuraPrEP(beanTest));
+		}
+		
+		@Test
+		@DisplayName("Configura una PrEP con fine nel passato: il protocollo non risulta attivo")
+		public void testConfiguraPrEPConFineNelPassatoVieneAnnullata() throws Exception {
+			fakeUtente("Utente4");
+			ProtocolloPrEPBean bean = new ProtocolloPrEPBean();
+			bean.setTipoPrEP(TipologiaPrEP.ON_DEMAND);
+			LocalDate dataInizioPassata = LocalDate.now().minusDays(10);
+			bean.setDataInizio(dataInizioPassata);
+			bean.setOrario(LocalTime.of(10, 0));
+			controller.configuraPrEP(bean);
+			DAOFactory factory = DAOFactoryProvider.getDAOFactory();
+			ProtocolloPrEPDAO dao = factory.createProtocolloPrEPDAO();
+			ProtocolloPrEP protocolloAttivo = dao.trovaProtocolloAttivo(utenteTest.getUsername());
+			assertEquals(false, protocolloAttivo.getStatoPrEP());
+		}
 }
