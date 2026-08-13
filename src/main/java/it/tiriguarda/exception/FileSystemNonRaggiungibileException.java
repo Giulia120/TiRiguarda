b67fc168ad4e66f19ac33af7744b0600e47d1e01
Giulia120 <giulsuni@gmail.com>
2026-08-13 10:30:50 +0200

    info

--- src/main/java/it/tiriguarda/exception/FileSystemNonRaggiungibileException.java
+++ src/main/java/it/tiriguarda/exception/FileSystemNonRaggiungibileException.java
@@ -1,6 +1,7 @@
 package it.tiriguarda.exception;
 
 public class FileSystemNonRaggiungibileException extends TiRiguardaException {
+
 	public FileSystemNonRaggiungibileException(String messaggio) {
         super(messaggio);
     }
