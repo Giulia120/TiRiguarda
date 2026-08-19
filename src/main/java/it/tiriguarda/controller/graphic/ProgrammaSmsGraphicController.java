4d86472475989a9d3fbb309a7b24752f52ad394f
Caterina7 <spinellicaterina11@gmail.com>
2026-08-19 10:57:21 +0200

    sms graphic controller

---
+++ src/main/java/it/tiriguarda/controller/graphic/ProgrammaSmsGraphicController.java
@@ -0,0 +1,17 @@
+package it.tiriguarda.controller.graphic;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class ProgrammaSmsGraphicController {
+	
+	@FXML private Button confermaButton;
+	@FXML private Label info;
+	@FXML private Button backButton;
+	@FXML private TextField oraSms;
+	@FXML private Label sottotitolo;
+	
+	
+}
